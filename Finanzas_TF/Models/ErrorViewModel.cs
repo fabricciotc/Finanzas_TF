622217efd18a5d9723593fbdaa923edb66546b7e
Fabriccio Tornero <u201713252@upc.edu.pe>
2021-08-28 18:51:15 -0500

    Agregar archivos de proyecto.

---
+++ Finanzas_TF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Finanzas_TF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
