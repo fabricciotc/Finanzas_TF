09ea925b1fb331ca7788b8f9f228002803cfacd9
Fabriccio Tornero <u201713252@upc.edu.pe>
2021-11-21 00:01:22 -0500

    mejoras de cotizacion sola

--- Finanzas_TF/Controllers/CotizadorController.cs
+++ Finanzas_TF/Controllers/CotizadorController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Excel.FinancialFunctions;
 using System.Threading.Tasks;
 
 namespace Finanzas_TF.Controllers
@@ -25,6 +26,13 @@ namespace Finanzas_TF.Controllers
             var applicationDbContext = await _context.ReciboHonorarios.Include(r => r.Cliente).Where(c=>c.FechaPago.Date> DateTime.Now.Date).ToListAsync();
             return View(applicationDbContext);
         }
+        [HttpGet]
+        public async Task<IActionResult> Index2(Guid re)
+        {
+            var applicationDbContext = await _context.ReciboHonorarios.Include(r => r.Cliente).Where(c => c.Id == re).ToListAsync();
+            ViewBag.code = re;
+            return View(applicationDbContext);
+        }
         [HttpPost]
         public async Task<IActionResult> Calculo(Calculador c)
         {
@@ -39,28 +47,86 @@ namespace Finanzas_TF.Controllers
             ViewBag.TEA = c.TEA;
             ViewBag.anio = c.Anio;
             ViewBag.dolar = c.Dolar;
-            var applicationDbContext =await _context.ReciboHonorarios.Include(r => r.Cliente).Where(c => c.FechaPago.Date > DateTime.Now.Date).ToListAsync();
+            var applicationDbContext = await _context.ReciboHonorarios.Include(r => r.Cliente).Where(c => c.FechaPago.Date > DateTime.Now.Date).ToListAsync();
             var ListFinal = new List<ReciboHonorariosCalculo>();
             foreach (var item in applicationDbContext)
             {
                 var tepTEM = Math.Pow((double)(1 + c.TEA), (double)((double)(item.FechaPago.Date - DateTime.Now.Date).TotalDays / (double)c.Anio)) / 100;
                 var dTem = (decimal)(tepTEM / (1 + tepTEM));
-                var monto = (item.Moneda==0?item.Monto:item.Monto*c.Dolar);
-                ListFinal.Add(new ReciboHonorariosCalculo { 
-                    NombreCliente=item.Cliente.RUC+" - "+item.Cliente.RazonSocial,
-                    Monto=monto,
-                    dias= Convert.ToInt32((item.FechaPago.Date - DateTime.Now.Date).TotalDays),
-                    gFinal=c.gFinal,
-                    gInicial=c.gInicio,
-                    TEP= Math.Round((decimal)tepTEM*100,2),
-                    d=Math.Round(dTem*100,2),
-                    Descuento= Math.Round(monto*dTem,2),
-                    ValorNeto= Math.Round(monto-(monto * dTem),2),
-                    ValorRecibir = Math.Round((monto - (monto * dTem))-c.gInicio,2),
-                    Flujo=monto-c.gFinal,
-                    FechaPago=item.FechaPago
+                var monto = (item.Moneda == 0 ? item.Monto : item.Monto * c.Dolar);
+                ListFinal.Add(new ReciboHonorariosCalculo {
+                    NombreCliente = item.Cliente.RUC + " - " + item.Cliente.RazonSocial,
+                    Monto = monto,
+                    dias = Convert.ToInt32((item.FechaPago.Date - DateTime.Now.Date).TotalDays),
+                    gFinal = c.gFinal,
+                    gInicial = c.gInicio,
+                    TEP = Math.Round((decimal)tepTEM * 100, 2),
+                    d = Math.Round(dTem * 100, 2),
+                    Descuento = Math.Round(monto * dTem, 2),
+                    ValorNeto = Math.Round(monto - (monto * dTem), 2),
+                    ValorRecibir = Math.Round((monto - (monto * dTem)) - c.gInicio, 2),
+                    Flujo =  c.gFinal- monto ,
+                    FechaPago = item.FechaPago
                 });
             }
+            var total = ListFinal.Sum(c => c.ValorRecibir);
+            var fechaHoy = DateTime.Now.Date;
+            var fechas = ListFinal.Select(c => c.FechaPago).ToList();
+            fechas.Insert(0,fechaHoy);
+            var flujos = ListFinal.Select(c => (double)c.Flujo).ToList();
+            flujos.Add((double)total);
+            double result = Financial.XIrr(flujos, fechas);
+            ViewBag.TCEA = Math.Round(result*-100,2);
+            return View(ListFinal);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Calculo2(Calculador c,Guid re)
+        {
+            if (c.Tasa == 1)
+            {
+                var div = (double)(c.TEA / 1200);
+                var tg = Math.Pow((double)(1 + (div)), 12.00) - 1;
+                c.TEA = (decimal)tg * 100;
+            }
+            ViewBag.gInicial = c.gInicio;
+            ViewBag.gFinal = c.gFinal;
+            ViewBag.TEA = c.TEA;
+            ViewBag.anio = c.Anio;
+            ViewBag.dolar = c.Dolar;
+            var applicationDbContext = await _context.ReciboHonorarios.Include(r => r.Cliente).Where(c => c.Id ==re).ToListAsync();
+            var ListFinal = new List<ReciboHonorariosCalculo>();
+            foreach (var item in applicationDbContext)
+            {
+                var tepTEM = Math.Pow((double)(1 + c.TEA), (double)((double)(item.FechaPago.Date - DateTime.Now.Date).TotalDays / (double)c.Anio)) / 100;
+                var dTem = (decimal)(tepTEM / (1 + tepTEM));
+                var monto = (item.Moneda == 0 ? item.Monto : item.Monto * c.Dolar);
+                ListFinal.Add(new ReciboHonorariosCalculo
+                {
+                    NombreCliente = item.Cliente.RUC + " - " + item.Cliente.RazonSocial,
+                    Monto = monto,
+                    dias = Convert.ToInt32((item.FechaPago.Date - DateTime.Now.Date).TotalDays),
+                    gFinal = c.gFinal,
+                    gInicial = c.gInicio,
+                    TEP = Math.Round((decimal)tepTEM * 100, 2),
+                    d = Math.Round(dTem * 100, 2),
+                    Descuento = Math.Round(monto * dTem, 2),
+                    ValorNeto = Math.Round(monto - (monto * dTem), 2),
+                    ValorRecibir = Math.Round((monto - (monto * dTem)) - c.gInicio, 2),
+                    Flujo = c.gFinal - monto,
+                    FechaPago = item.FechaPago
+                });
+            }
+            var total = ListFinal.Sum(c => c.ValorRecibir);
+            var fechaHoy = DateTime.Now.Date;
+            var fechas = ListFinal.Select(c => c.FechaPago).ToList();
+            fechas.Insert(0, fechaHoy);
+            var flujos = ListFinal.Select(c => (double)c.Flujo).ToList();
+            flujos.Add((double)total);
+            double result = Financial.XIrr(flujos, fechas);
+            ViewBag.TCEA = Math.Round(result * -100, 2);
             return View(ListFinal);
         }
     }
